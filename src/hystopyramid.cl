ceed8c548a47cc0e058ee456fbdc1243a4970d1c
Paul Loyd <pavelko95@gmail.com>
2019-03-03 16:05:17 +0300

    Fix the hystopyramid and specify ntimes

--- src/hystopyramid.cl
+++ src/hystopyramid.cl
@@ -35,40 +35,33 @@ kernel void make_grid(
     global const point_t *restrict points,
     global float *restrict grid
 ) {
-    printf("MKGRD GID=%d LID=%d GR=%d\n", get_global_id(0), get_local_id(0), get_group_id(0));
-
     int gid = get_global_id(0);
 
     point_t point = points[gid];
 
+    float2 cell_sz = (bbox.zw - bbox.xy) / (float)size;
+
     float2 relative = clamp(point.xy, bbox.xy, bbox.zw) - bbox.xy;
-    uint2 coords = convert_uint2(relative / (float)size);
+    uint2 coords = convert_uint2(relative / cell_sz);
 
     uint idx = 4 * c2id(coords, size);
 
     atomic_add_float(grid + idx + 0, point.x);
     atomic_add_float(grid + idx + 1, point.y);
     atomic_add_float(grid + idx + 2, point.z);
-    atomic_add_float(grid + idx + 3, 1.f);
+    atomic_add_float(grid + idx + 3, 1.0f);
 }
 
 kernel void make_level(
     global const cell_t *restrict top,
     global cell_t *restrict current
 ) {
-    printf("MKLVL GID=%d:%d LID=%d:%d GR=%d:%d\n",
-            get_global_id(0), get_global_id(1),
-            get_local_id(0), get_local_id(1),
-            get_group_id(0), get_group_id(1));
-
     uint cur_size = get_global_size(0);
     uint2 cur_coords = (uint2)(get_global_id(0), get_global_id(1));
     uint cur_id = c2id(cur_coords, cur_size);
 
     uint2 top_coords = 2 * cur_coords;
     uint top_size = 2 * cur_size;
-
-    printf("%2d <- %2d\n", cur_coords, top_coords);
 
     current[cur_id] = top[c2id(top_coords + (uint2)(0, 0), top_size)]
                     + top[c2id(top_coords + (uint2)(0, 1), top_size)]
--- src/hystopyramid.py
+++ src/hystopyramid.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 
+import unittest
 from os import path
 
 import numpy as np
 import pyopencl as cl
+from numpy.testing import assert_allclose, assert_array_equal
 from pyopencl import cltypes
 
 DIRNAME = path.abspath(path.dirname(__file__))
@@ -15,24 +17,30 @@ with open(path.join(DIRNAME, "hystopyramid.cl"), "r") as file:
 CELL_NBYTES = cltypes.float4.itemsize
 
 
-def make_levels(depth, pyramid_g):
+def float4(x, y, z, w):  # pylint: disable=invalid-name
+    return cltypes.make_float4(x, y, z, w)
+
+
+def make_levels(depth, pyramid_np, pyramid_g):
     origin = 0
     size = 1
 
     levels = []
 
     for _ in range(depth):
-        sub = pyramid_g[CELL_NBYTES * origin : CELL_NBYTES * size * size]
+        end = origin + size * size
+        level_np = pyramid_np[origin:end]
+        level_g = pyramid_g[CELL_NBYTES * origin : CELL_NBYTES * end]
 
-        levels.append(sub)
+        levels.append((level_np, level_g))
 
-        origin += size
+        origin = end
         size *= 2
 
     return levels
 
 
-def run(depth, bbox, points):
+def run(depth, bbox, points, ntimes=2):
     grid_size = 2 ** (depth - 1)
     pyramid_ncells = int((4 ** depth - 1) / 3)
     pyramid_nbytes = CELL_NBYTES * pyramid_ncells
@@ -43,6 +51,9 @@ def run(depth, bbox, points):
     ctx = cl.create_some_context()
     prg = cl.Program(ctx, HYSTOPYRAMID_CL).build(["-cl-std=CL2.0"])
 
+    make_grid_krn = prg.make_grid
+    make_level_krn = prg.make_level
+
     queue = cl.CommandQueue(
         ctx, properties=cl.command_queue_properties.PROFILING_ENABLE
     )
@@ -51,46 +62,121 @@ def run(depth, bbox, points):
     points_g = cl.Buffer(ctx, mem.READ_ONLY | mem.COPY_HOST_PTR, hostbuf=points_np)
     pyramid_g = cl.Buffer(ctx, mem.READ_WRITE, pyramid_nbytes)
 
-    cl.enqueue_fill_buffer(queue, pyramid_g, cltypes.float(0), 0, pyramid_nbytes)
+    levels = make_levels(depth, pyramid_np, pyramid_g)
 
-    levels = make_levels(depth, pyramid_g)
+    events = []
 
-    ev_grid = prg.make_grid(
-        queue,
-        points_np.shape,
-        None,
-        cltypes.make_float4(*bbox),
-        cltypes.uint(grid_size),
-        points_g,
-        levels[-1],
-    )
+    for _ in range(ntimes):
+        cl.enqueue_fill_buffer(queue, pyramid_g, cltypes.float(0), 0, pyramid_nbytes)
+
+        ev_grid = make_grid_krn(
+            queue,
+            points_np.shape,
+            None,
+            float4(*bbox),
+            cltypes.uint(grid_size),
+            points_g,
+            levels[-1][1],
+        )
+
+        events.append(ev_grid)
+
+        for lvl in range(len(levels) - 1, 0, -1):
+            top_g = levels[lvl][1]
+            cur_g = levels[lvl - 1][1]
+            size = 2 ** (lvl - 1)
+            ev_lvl = make_level_krn(queue, (size, size), None, top_g, cur_g)
+            events.append(ev_lvl)
+
+        queue.finish()
 
     cl.enqueue_copy(queue, pyramid_np, pyramid_g)
-    print(pyramid_np[: grid_size * grid_size], len(levels))
 
-    events = [ev_grid]
+    # rel = lambda x: (x - events[0].profile.start) * 1e-6
+    # pure_spent = 0
+    # print("QUEUE SUB START END SPENT")
+    # for ev in events:
+    # print(
+    # "{:.2f} {:.2f} {:.2f} {:.2f} {:.2f}".format(
+    # rel(ev.profile.queued),
+    # rel(ev.profile.submit),
+    # rel(ev.profile.start),
+    # rel(ev.profile.end),
+    # (ev.profile.end - ev.profile.start) * 1e-6,
+    # )
+    # )
 
-    for lvl in range(len(levels) - 1, 0, -1):
-        top = levels[lvl]
-        cur = levels[lvl - 1]
-        size = 2 ** (lvl - 1)
-        ev_lvl = prg.make_level(queue, (size, size), None, cur.shape, top, cur)
-        events.append(ev_lvl)
+    # pure_spent += (ev.profile.end - ev.profile.start) * 1e-6
 
-    cl.enqueue_copy(queue, pyramid_np, pyramid_g)
+    spent = (events[-1].profile.end - events[0].profile.start) * 1e-6
 
-    spent = (events[-1].profile.end - ev_grid.profile.start) * 1e-6
+    # print("pure: %f\nspent: %f" % (pure_spent, spent))
 
-    return pyramid_np, spent
+    return [lvl[0] for lvl in levels], spent
+
+
+class TestHystopyramid(unittest.TestCase):
+    def test_single(self):
+        zero = float4(0.0, 0.0, 0.0, 0.0)
+
+        pyramid, _ = run(3, (0, 0, 1, 1), [(0.3, 0.7, 2.0)])
+        self.assertEqual(len(pyramid), 3)
+        assert_array_equal(pyramid[0], [float4(0.3, 0.7, 2.0, 1.0)])
+        assert_array_equal(pyramid[1], [zero, zero, float4(0.3, 0.7, 2.0, 1.0), zero])
+
+        pyramid, _ = run(3, (0, 0, 1, 1), [(0.3, 0.3, 2.0)])
+        self.assertEqual(len(pyramid), 3)
+        assert_array_equal(pyramid[0], [float4(0.3, 0.3, 2.0, 1.0)])
+        assert_array_equal(pyramid[1], [float4(0.3, 0.3, 2.0, 1.0), zero, zero, zero])
+
+        pyramid, _ = run(3, (0, 0, 1, 1), [(0.7, 0.7, 2.0)])
+        self.assertEqual(len(pyramid), 3)
+        assert_array_equal(pyramid[0], [float4(0.7, 0.7, 2.0, 1.0)])
+        assert_array_equal(pyramid[1], [zero, zero, zero, float4(0.7, 0.7, 2.0, 1.0)])
+
+        pyramid, _ = run(3, (0, 0, 1, 1), [(0.7, 0.3, 2.0)])
+        self.assertEqual(len(pyramid), 3)
+        assert_array_equal(pyramid[0], [float4(0.7, 0.3, 2.0, 1.0)])
+        assert_array_equal(pyramid[1], [zero, float4(0.7, 0.3, 2.0, 1.0), zero, zero])
+
+    def test_all_in(self):
+        pyramid, _ = run(
+            1, (0, 0, 1, 1), [(0.3, 0.2, 2.0), (0.7, 0.8, 1.0), (0.5, 0.5, 1.5)]
+        )
+
+        self.assertEqual(len(pyramid), 1)
+        assert_array_equal(pyramid[0], [float4(1.5, 1.5, 4.5, 3.0)])
+
+    def test_random(self):
+        points = np.random.rand(100, 3)
+
+        pyramid, _ = run(4, (0, 0, 1, 1), points)
+
+        self.assertEqual(len(pyramid), 4)
+
+        lhs = tuple(pyramid[0][0])
+        rhs = (*points.sum(0), 100.0)
+
+        self.assertTrue(np.allclose(lhs, rhs))
 
 
 def main():
-    points = [(0.1, 0.1, 1), (0.9, 0.9, 1), (0.6, 0.6, 1)]
+    # points = [(0.1, 0.1, 1.0), (0.9, 0.9, 1.0), (0.6, 0.6, 1.0), (0.2, 0.2, 1.0)]
 
-    (pyramid_np, spent) = run(1, (0, 0, 1, 1), points)
+    points = np.random.rand(100, 3)
 
-    print(pyramid_np, spent)
+    (levels, spent) = run(4, (0, 0, 1, 1), points, 10)
+
+    # for i, level in enumerate(levels):
+    # np.set_printoptions(linewidth=20 * (2 ** i))
+
+    # print("~~~~~~~~~~~~")
+    # print(level)
+
+    # np.set_printoptions(linewidth=75)
+    print(spent)
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
+    # main()
